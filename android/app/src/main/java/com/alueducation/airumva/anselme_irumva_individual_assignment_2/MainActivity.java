6cb1e1593bee514edbd5a943198706f545e0fab6
Anselme Irumva <andesanselme@gmail.com>
2025-07-06 17:39:14 +0200

    initiL commit

---
+++ android/app/src/main/java/com/alueducation/airumva/anselme_irumva_individual_assignment_2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alueducation.airumva.anselme_irumva_individual_assignment_2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
